ebd3462769b30d3f12c7ec993e27efbfc3583b85
RIKU <rank228130@gmail.com>
2021-07-10 22:41:55 +0900

    update: board add

--- src/App.vue
+++ src/App.vue
@@ -3,56 +3,19 @@
     <div class="container">
       <h1>Todoリスト</h1>
       <img alt="Vue logo" src="./assets/logo.png" />
-      <addTodo @submitValue="addTodo" />
-      <editTodo v-if="isEditing" :targetTodoItem="targetTodoItem" @edit="edit" />
-      <todos :todos="todos" @deleteTodo="deleteTodo" @editTodoHandler="editTodoHandler" />
+      <board />
     </div>
   </div>
 </template>
 
 <script>
-import AddTodo from "@/components/AddTodo.vue";
-import editTodo from "@/components/editTodo.vue";
-import Todos from "@/components/Todos.vue";
+import Board from "@/components/Board.vue";
 
 export default {
   name: "App",
   components: {
-    Todos,
-    AddTodo,
-    editTodo,
-  },
-  data() {
-    return {
-      todos: [],
-      isEditing: false,
-      targetTodoItem: null,
-    };
-  },
-  methods: {
-    addTodo(inputValue, id) {
-      this.todos.push({
-        todo: inputValue,
-        id,
-      });
-    },
-    deleteTodo(targetTodoId) {
-      this.todos = this.todos.filter((todo) => todo.id !== targetTodoId);
-      this.isEditing = false;
-    },
-    editTodoHandler(targetTodoId) {
-      this.isEditing = true;
-      this.targetTodoItem = this.todos.find((todo) => todo.id === targetTodoId);
-    },
-    edit(editTodoItemValue, targetTodoItemId) {
-      this.todos.forEach((todo) => {
-        if (todo.id === targetTodoItemId) {
-          todo.todo = editTodoItemValue;
-          this.isEditing = false;
-        }
-      });
-    },
-  },
+    Board,
+  }
 };
 </script>
 
